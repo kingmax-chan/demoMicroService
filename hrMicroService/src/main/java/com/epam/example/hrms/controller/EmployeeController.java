2ff97352d911fe555ab82d735be08044bcfffc6c
Nicolas_Wu <nicolas_wu@epam.com>
2016-12-07 17:43:42 +0800

    HR MS logic

--- hrMicroService/src/main/java/com/epam/example/hrms/controller/EmployeeController.java
+++ hrMicroService/src/main/java/com/epam/example/hrms/controller/EmployeeController.java
@@ -1,5 +1,29 @@
 package com.epam.example.hrms.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.epam.example.hrms.model.Employee;
+import com.epam.example.hrms.service.IEmployeeService;
+
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+	@Autowired
+	private IEmployeeService employeeService;
+	
+	@GetMapping("/{employeeId}")
+	public Employee findEmployeeById(@PathVariable String employeeId){
+		return employeeService.findEmployee(employeeId);
+	}
+	
+	@GetMapping("/all")
+	public List<Employee> findEmployees(){
+		return employeeService.findAllEmployees();
+	}
 }
